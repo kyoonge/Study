026f9dd9efa5de1130a98119dd1e3b219cb5c10b
kyoonge <kyoonge98@gmail.com>
2023-02-08 23:47:42 +0900

    퀵2

--- ECT/6/review1.py
+++ ECT/6/review1.py
@@ -71,3 +71,19 @@ def quick_sort(array,start,end):
 
 quick_sort(array,0,len(array)-1)
 print(array)
+
+#6-5
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+  if len(array)<=1:
+    return array
+
+  pivot = array[0]
+  tail = array[1:]
+  left = [x for x in tail if x<=pivot]
+  right = [x for x in tail if x>pivot]
+
+  return quick_sort(left)+[pivot]+quick_sort(right)
+
+print(quick_sort(array))
