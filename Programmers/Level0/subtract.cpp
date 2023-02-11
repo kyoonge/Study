cfd21ceca26887dcb399af2fdaa67eee18e131ab
kyoonge <kyoonge98@gmail.com>
2023-02-11 11:24:15 +0900

    두 수의 차

---
+++ Programmers/Level0/subtract.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    
+    answer =num1-num2;
+    
+    return answer;
+}
+
+int main(){
+  int n1=2;
+  int n2=3;
+  
+  cout<<solution(n1,n2);
+}
