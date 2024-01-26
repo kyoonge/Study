599405cf33f09b98021f4d09c30a1ed99941d309
Minkyung <96893605+kyoonge@users.noreply.github.com>
2024-01-27 01:01:49 +0900

    [level 2] Title: 구명보트, Time: 1.62 ms, Memory: 4.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int a, int b){
+    return a>b;
+}
+
+int solution(vector<int> people, int limit) {
+    
+    int answer = 0;
+    int rIndex = people.size()-1;
+    
+    sort(people.begin(), people.end(), cmp);
+    
+    for(int i = 0; i<=rIndex; i++){
+    
+        answer += 1;
+        if(people[i] + people[rIndex] <= limit){
+            rIndex--;
+        }
+        
+    }
+    
+    return answer;
+}
+
